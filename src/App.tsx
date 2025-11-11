2f0279e679937aa021ed61f25cfeb27167c334d2
Paul Adriaanse <pea.adriaanse@gmail.com>
2025-11-11 22:14:18 +0100

    Start on page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Photo from "./assets/pasfoto_half.png"
+import GitHub from "./assets/github.png"
+import LinkedIn from "./assets/linked_in.png"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <head>
+      <title>Paul Adriaanse | Portfolio</title>
+      <img src={Photo} alt="profile picture" className="top_banner"/>
+      <h1>
+        Paul Adriaanse
+      </h1>
+      <div> {/* nav or address (add email) */}
+        <a href="https://github.com/pea-adriaanse">
+          <img src={GitHub} alt="github logo" />
+          GitHub
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.linkedin.com/in/paul-adriaanse/">
+          <img src={LinkedIn} alt="linkedin logo" />
+          LinkedIn
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </head>
+    <body>
+      {}
+    </body>
     </>
-  )
+  );
+  
 }
 
 export default App
